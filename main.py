9e41a62f254c70f3a80b6057d35d7abad21d1d0c
EvanSat <53355852+EvanSat@users.noreply.github.com>
2022-10-06 04:31:25 -0500

    Update main.py
    Basic search of google's 'search' box and provides a test input

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 s = Service(PATH)
@@ -7,3 +10,9 @@ browser = webdriver.Chrome(service=s)
 
 url = "https://www.google.com"
 browser.get(url)
+print(browser.title)
+
+search = browser.find_element(By.NAME, 'q')  # Finds Search Box
+search.send_keys("test imput")
+search.send_keys(Keys.RETURN)
+# browser.close()     # closes tab |.quit() will close whole browser
